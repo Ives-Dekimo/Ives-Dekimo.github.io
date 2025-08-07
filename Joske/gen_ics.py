ca6ac1eee064b1976c6e361c7d5f623a5554a44c
Ives van der Flaas <Ives.vanderflaas@dekimo.com>
2025-08-07 13:21:36 +0200

    Update calendars

--- Joske/gen_ics.py
+++ Joske/gen_ics.py
@@ -26,14 +26,13 @@ print(f"Current week number: {current_week_number}")
 
 idx = 0 
 
-calendar = Calendar()
-
 def get_monday_of_week(year, week):
     return datetime.fromisocalendar(year, week, 1)
 
 weeks_next_year = 53 if (current_year % 4 == 0 and current_year % 100 != 0) or (current_year % 400 == 0) else 52
 
-def do_filtered(employees, current_year, weeks_this_year, first_in_cycle_week_number, idx, calendar, get_monday_of_week, weeks_next_year, filter):
+def do_filtered(employees, current_year, weeks_this_year, first_in_cycle_week_number, idx, get_monday_of_week, weeks_next_year, filter):
+    calendar = Calendar()
     for week in range(first_in_cycle_week_number, weeks_this_year + 1):
         joske = employees[idx % len(employees)]
 
@@ -73,8 +72,8 @@ def do_filtered(employees, current_year, weeks_this_year, first_in_cycle_week_nu
     with open(f"calendars/joske_{filter or 'all'}.ics", "w") as f:
         f.writelines(calendar)
 
-do_filtered(employees, current_year, weeks_this_year, first_in_cycle_week_number, idx, calendar, get_monday_of_week, weeks_next_year, None)
+do_filtered(employees, current_year, weeks_this_year, first_in_cycle_week_number, idx, get_monday_of_week, weeks_next_year, None)
 
 for employee in employees:
-    do_filtered(employees, current_year, weeks_this_year, first_in_cycle_week_number, idx, calendar, get_monday_of_week, weeks_next_year, employee)
+    do_filtered(employees, current_year, weeks_this_year, first_in_cycle_week_number, idx, get_monday_of_week, weeks_next_year, employee)
 
