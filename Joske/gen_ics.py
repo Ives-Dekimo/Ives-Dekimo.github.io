5982e76b2ff799c264333110d53e32cdf66492b7
Ives van der Flaas <Ives.vanderflaas@dekimo.com>
2025-08-07 14:25:28 +0200

    Fix tz

--- Joske/gen_ics.py
+++ Joske/gen_ics.py
@@ -29,16 +29,19 @@ idx = 0
 
 def get_monday_of_week(year, week):
     dt = datetime.fromisocalendar(year, week, 1)
-    dt_utc = dt.replace(tzinfo=pytz.utc)
-    # to local timezone
-    local_tz = pytz.timezone('Europe/Brussels')  # Replace with your local timezone
-    dt_local = dt_utc.astimezone(local_tz)
+
+    # Assume the time is at midnight and UTC
+    dt_utc = pytz.timezone('Europe/Brussels').localize(dt)
+
+    # Convert to local timezone (e.g., system local)
+    dt_local = dt_utc.astimezone(pytz.utc)
     return dt_local
 
 weeks_next_year = 53 if (current_year % 4 == 0 and current_year % 100 != 0) or (current_year % 400 == 0) else 52
 
 def do_filtered(employees, current_year, weeks_this_year, first_in_cycle_week_number, idx, get_monday_of_week, weeks_next_year, filter):
-    print(f"Generating calendar for filter: {filter or 'all'}")	
+    print()
+    print(f"-- Generating calendar for filter: {filter or 'all'}")	
 
     calendar = Calendar()
     for week in range(first_in_cycle_week_number, weeks_this_year + 1):
@@ -46,7 +49,7 @@ def do_filtered(employees, current_year, weeks_this_year, first_in_cycle_week_nu
 
         if filter == None or joske == filter:
             start = get_monday_of_week(current_year, week)
-            end = start + timedelta(days=6)
+            end = start + timedelta(days=7)
 
             print(f"Week number: {week}, Year: {current_year}, Joske: {joske}")
             print(f"Start: {start}, End: {end}")	
@@ -64,7 +67,7 @@ def do_filtered(employees, current_year, weeks_this_year, first_in_cycle_week_nu
 
         if filter == None or joske == filter:
             start = get_monday_of_week(current_year + 1, week)
-            end = start + timedelta(days=6)
+            end = start + timedelta(days=7)
 
             print(f"Week number: {week}, Year: {current_year+1}, Joske: {joske}")
             print(f"Start: {start}, End: {end}")
